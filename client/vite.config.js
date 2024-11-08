faef840561e202121ecde5453fe0fb1d81dd768e
aman <amansinghmail07@gmail.com>
2024-11-08 22:09:21 +0530

    deploying frontend

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensures the output directory is 'dist'
+  },
   server: {
     proxy: {
       '/api': {
         target: 'https://eventhub-backend-14dq.onrender.com',
         changeOrigin: true,
+        secure: false, // Useful for dealing with self-signed certificates in development
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
