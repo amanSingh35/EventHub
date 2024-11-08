5212fe41635ca8a6d9bda554e5190e780d42c8af
aman <amansinghmail07@gmail.com>
2024-11-09 02:19:08 +0530

    vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   build: {
     outDir: 'dist', // Ensures the output directory is 'dist'
   },
-  server: {
+  server: mode === 'development' ? {
     proxy: {
       '/api': {
         target: 'https://eventhub-backend-14dq.onrender.com',
         changeOrigin: true,
-        secure: false, // Useful for dealing with self-signed certificates in development
+        secure: false,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
-  },
-});
+  } : undefined,
+}));
+
